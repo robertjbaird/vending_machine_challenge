07321f79c66b4c7034631ce5fb0ff1dc12868fda
Robert Baird <robert.j.baird@gmail.com>
2016-01-28 18:28:43 -0500

    autoscroll

--- leaderboard.js
+++ leaderboard.js
@@ -1,4 +1,8 @@
 if (Meteor.isClient) {
+  RouterAutoscroll.animationDuration = 0;
+
+
+
   Template.leaderboard.helpers({
     players: function () {
       return Players.find({}, { sort: { score: -1, name: 1 } });
